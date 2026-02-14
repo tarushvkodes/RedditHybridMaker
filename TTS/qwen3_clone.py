c4e8780348c5668a21246ebf30bb195f8edfa94e
Tarushv Kosgi <tarushv.kosgi@outlook.com>
2026-02-14 12:33:23 -0500

    chore(tts): restore WSL default execution now that sox is installed

--- TTS/qwen3_clone.py
+++ TTS/qwen3_clone.py
@@ -29,8 +29,7 @@ class Qwen3Clone:
             "QWEN3_SOX_DIR",
             r"C:\Users\tarus\AppData\Local\Microsoft\WinGet\Packages\ChrisBagwell.SoX_Microsoft.Winget.Source_8wekyb3d8bbwe\sox-14.4.2",
         )
-        # Default to native Windows execution to avoid WSL SoX dependency issues.
-        self.use_wsl = os.getenv("QWEN3_USE_WSL", "0").strip().lower() in {"1", "true", "yes"}
+        self.use_wsl = os.getenv("QWEN3_USE_WSL", "1").strip().lower() in {"1", "true", "yes"}
         self.wsl_distro = os.getenv("QWEN3_WSL_DISTRO", "Ubuntu-24.04")
         self.wsl_python = os.getenv("QWEN3_WSL_PYTHON", "/home/tarushv/.venvs/rhm/bin/python")
 
